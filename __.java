e1043cd9f6f20866e26afcfb0266e6f4615ebb32
HoaQuyingVN <126839568+HoaQuyingVN@users.noreply.github.com>
2023-03-04 14:48:10 +0700

    Create __.java

---
+++ __.java
@@ -0,0 +1,32 @@
+package com.freeOS
+// Import packages 
+import com.freeOS.search;
+import com.freeOS.searchTerm;
+import com.freeOS.searchBox;
+import com.freeOS.searchApp;
+import com.freeOS.osHeat;
+import com.freeOS.androidTV;
+import com.freeOS.dir;
+import com.freeOS.box;
+import com.freeOS.appstore;
+import com.freeOS.note;
+import com.freeOS.heatNote;
+import com.freeOS.system;
+import com.freeOS.systemAlert;
+import com.freeOS.alert;
+import com.freeOS.eas;
+import com.freeOS.popup;
+import com.freeOS.notice;
+import com.freeOS.boxt;
+import com.freeOS.dirPerm;
+import com.freeOS.permission;
+import com.freeOS.systemData;
+import com.freeOS.ram;
+import com.freeOS.shutdown;
+import com.freeOS.restart;
+// internet speed and speed download
+public void class Main([request.eta, request.wifi.speed]){
+    systemData[eta].Tests(wifi)
+    ram(1000000/200*152);
+    boxt('Progress', eta, wifi)
+}
